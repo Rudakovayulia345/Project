ff1efd75baed1f5e036976f3e5b212114783ac9b
Rudakovayulya345 <yea2107@gmail.com>
2023-06-12 21:08:03 +0300

    comment

---
+++ Java Intro/second_sample.js
@@ -0,0 +1,33 @@
+/*var n = 123; //целое число
+n = 12.345; // дробное число
+var b = 1 / 0; // infinity
+var c = "Число" / 2; // NaN - not a number (не является числом
+var str = "мама мыла раму";
+var s = 'Одинарные кавычки';
+var letter = 'a';
+var checked = true; //истина
+var notchecked = false; // не совсем истина
+var user = {
+    name: "Вася",
+    sex: "Мужской",
+    height: 180,
+    weight: 60.6
+};
+alert(user.sex)
+var age = null;
+alert(age);//выведет null
+age = 10; // alert (age) выведет 10
+var x;
+alert(x); // выведет undefined
+x = 10;
+alert(x); //выведет 10
+const bigint = 2121236546847687463541369476341365948+94354158464135847n;*/
+console.log(typeof 'Students Lab'); //string
+console.log(typeof true); //boolean
+console.log(typeof tunderfined); 
+console.log(typeof Symbol()); 
+console.log(typeof {}); //object
+console.log(typeof []); //object
+console.log(typeof null); //object
+console.log(typeof function(){}); //function
+
